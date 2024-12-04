510a09a500536112ab89508b9573627cb6b75de6
Dani V <vaithid1@mskcc.org>
2024-12-04 15:15:27 -0500

    check py

--- extcode/check.py
+++ extcode/check.py
@@ -4,7 +4,7 @@ import csv
 input_file = "/home/nfs/vaithid1/FACETS/RewriteSnpPileup/outputs/TestGzipped.csv.gz"
 
 
-with gzip.open("/home/nfs/vaithid1/FACETS/RewriteSnpPileup/outputs/TestPythonOut.gz", "rt") as f:
+with gzip.open("/home/nfs/vaithid1/FACETS/RewriteSnpPileup/outputs/TestPythonOut_compArgs.gz", "rt") as f:
     for i, line in enumerate(f):
         print(line.strip())
         if i == 10:  # Stop after the 5th line (index 4 is the 5th line)
