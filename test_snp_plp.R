ec215a8b985aa82f16dd6bd8f9d2d2ebc1f9a4ac
Dani V <vaithid1@mskcc.org>
2024-12-04 14:15:21 -0500

    Checked gzipped file and it works!

--- test_snp_plp.R
+++ test_snp_plp.R
@@ -59,22 +59,5 @@ unlink(temp_makevars)
 sink()
 cat("unsunk")
 
-## check out results for funsies
-##source("/home/nfs/vaithid1/FACETS/RewriteSnpPileup/lookAtOutput.R")
-# output = read.csv(outfile)
-
-# ## output columns
-# ## chromosome position ref alt  then for each file there is an R E A D
-
-# output.long = pivot_longer(output, cols = !c("Chromosome", "Position", "Ref", "Alt"), names_to = c("file", ".value"),  # `file` becomes one column, RAED stay as values
-# names_pattern = "File(\\d+)([RAED])"  # Regex to separate file number and RAED
-# ) %>%
-#   mutate(across(
-#     .cols = -c(Chromosome, Position, Ref, Alt),  # Exclude Ref and Alt columns
-#     .fns = as.numeric      # Apply as.numeric
-#   )) %>% tbl_summary(
-#     by = file,
-#     digits = all_continuous() ~ 1,
-#     statistic = all_continuous() ~ "{mean}, ({sd})"
-#   )
+## check out results for funsies -> gzipped file
 
