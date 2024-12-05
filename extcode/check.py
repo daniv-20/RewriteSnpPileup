56c669ee9b828710ab973eb105fe2ec81b3ea593
Dani V <vaithid1@mskcc.org>
2024-12-05 09:15:50 -0500

    Because the encoding for the snp-plp.py is a bit odd (at least it doesn't seem to want to unzip nicely) I made a tool. Also made a venv but let's not upload that pls

--- extcode/check.py
+++ extcode/check.py
@@ -1,12 +1,16 @@
 import gzip
 import csv
+import pandas as pd
 
-input_file = "/home/nfs/vaithid1/FACETS/RewriteSnpPileup/outputs/TestGzipped.csv.gz"
+# input_file = "/home/nfs/vaithid1/FACETS/RewriteSnpPileup/outputs/TestGzipped.csv.gz"
 
 
-with gzip.open("/home/nfs/vaithid1/FACETS/RewriteSnpPileup/outputs/TestPythonOut_compArgs.gz", "rt") as f:
-    for i, line in enumerate(f):
-        print(line.strip())
-        if i == 10:  # Stop after the 5th line (index 4 is the 5th line)
-            break
+# with gzip.open("/home/nfs/vaithid1/FACETS/RewriteSnpPileup/outputs/TestPythonOut_compArgs.gz", "rt") as f:
+#     for i, line in enumerate(f):
+#         print(line.strip())
+#         if i == 10:  # Stop after the 5th line (index 4 is the 5th line)
+#             break
 
+py = pd.read_csv("TestPythonOut_compArgs.csv")
+
+r = pd.read_csv("../outputs/TestGzipped.csv")
