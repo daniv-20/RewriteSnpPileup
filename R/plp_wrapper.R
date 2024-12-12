9ef8646c66a055690da968dd9394a32e000b8e77
Dani V <vaithid1@mskcc.org>
2024-12-12 15:22:51 -0500

    not a great wrapper but it's a start

--- R/plp_wrapper.R
+++ R/plp_wrapper.R
@@ -4,12 +4,12 @@
 #'
 #' @param file Input file path (e.g., VCF file).
 #' @param output Output file path.
-#' @param threads Number of threads to use. Default is 1.
+#' @param bamfiles Vector of bamfile paths. 
 #' @return None. Results are written to the output file.
 #' @export
-run_snp_pileup <- function(vcffile, 
-  output, 
-  bamfiles, 
+run_snp_pileup <- function(vcffile, ## vcf file path
+  output, ## output file path
+  bamfiles, ## vector of file paths
   count_orphans = FALSE,
 gzipped = FALSE,
 ignore_overlaps = FALSE,
@@ -22,8 +22,8 @@ psuedo_snps = 0,
 verbose = FALSE,
 debug_mode = FALSE
 ) {
-    if (!file.exists(file)) stop("Input file does not exist: ", file)
-    if (threads <= 0) stop("Threads must be greater than 0.")
+    if (!file.exists(vcffile)) stop("Input file does not exist: ", vcffile)
+    ##if (threads <= 0) stop("Threads must be greater than 0.")
   
     qual_args = c("-Q", min_base_quality, 
     "-q", min_map_quality, 
@@ -50,7 +50,6 @@ debug_mode = FALSE
     qual_args = c(qual_args, "-d")
   }
     
-    # Convert to a list and call the underlying C++ function
     args <- c(qual_args, vcffile, output, bamfiles)
     .Call("_snp_plp_run_snp_pileup_logic", args)
 }
