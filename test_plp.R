64ff209f57ac112b7085f2ce078ef9240e6f1b7a
Dani V <vaithid1@mskcc.org>
2024-12-16 11:42:03 -0500

    testing snp-plp wrapper function

---
+++ test_plp.R
@@ -0,0 +1,10 @@
+devtools::document()
+
+setwd("/home/nfs/vaithid1/FACETS/RewriteSnpPileup")
+
+datapath <- "/ebfs/epibio/seshanv/snp-pileup"
+
+vcf <- "/usr/local/share/VCF/common_all_20180418_dedup_bg.vcf.gz"
+bams <- c(file.path(datapath, "HCC1143_BC10.bam"), file.path(datapath, "HCC1143_BL10.bam"))
+
+snp.plp::run_snp_pileup(vcffile = vcf, output = "test_test2.csv", bamfiles = bams, verbose = TRUE, debug_mode = TRUE, progress = TRUE)
