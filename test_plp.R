aa6158c0dbc7f4120cf483efb48abce770c09f27
Dani V <vaithid1@mskcc.org>
2024-12-16 11:48:41 -0500

    no need to return the args.

--- test_plp.R
+++ test_plp.R
@@ -7,7 +7,7 @@ datapath <- "/ebfs/epibio/seshanv/snp-pileup"
 vcf <- "/usr/local/share/VCF/common_all_20180418_dedup_bg.vcf.gz"
 bams <- c(file.path(datapath, "HCC1143_BC10.bam"), file.path(datapath, "HCC1143_BL10.bam"))
 
-snp.plp::run_snp_pileup(vcffile = vcf, output = "test_test2.csv", bamfiles = bams, verbose = TRUE, debug_mode = TRUE, progress = TRUE)
+snp.plp::run_snp_pileup(vcffile = vcf, output = "test_test3.csv", bamfiles = bams, verbose = TRUE, debug_mode = TRUE, progress = TRUE, gzipped = TRUE)
 
 
 library(tidyverse)
