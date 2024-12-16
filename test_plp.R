9a439a01daada18442e1ea56462d4c992cd5fd9e
Dani V <vaithid1@mskcc.org>
2024-12-16 11:48:09 -0500

    the wrapper function works for regular and gzipped outputs!

--- test_plp.R
+++ test_plp.R
@@ -8,3 +8,10 @@ vcf <- "/usr/local/share/VCF/common_all_20180418_dedup_bg.vcf.gz"
 bams <- c(file.path(datapath, "HCC1143_BC10.bam"), file.path(datapath, "HCC1143_BL10.bam"))
 
 snp.plp::run_snp_pileup(vcffile = vcf, output = "test_test2.csv", bamfiles = bams, verbose = TRUE, debug_mode = TRUE, progress = TRUE)
+
+
+library(tidyverse)
+library(gt)
+out <- read.csv("test_test2.csv")
+
+head(out) %>% gt()
