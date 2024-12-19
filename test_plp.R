3a56fc0080eafe7f1835f9a6b604d4bb88915fcd
Dani V <vaithid1@mskcc.org>
2024-12-19 12:04:16 -0500

    no .o's pls

--- test_plp.R
+++ test_plp.R
@@ -1,5 +1,7 @@
 devtools::document()
 
+devtools::install_github("daniv-20/RewriteSnpPileup", ref = "gh_build")
+
 setwd("/home/nfs/vaithid1/FACETS/RewriteSnpPileup")
 
 datapath = "/ebfs/epibio/seshanv/snp-pileup"
@@ -7,4 +9,8 @@ datapath = "/ebfs/epibio/seshanv/snp-pileup"
 vcf = "/usr/local/share/VCF/common_all_20180418_dedup_bg.vcf.gz"
 bams = c(file.path(datapath, "HCC1143_BC10.bam"), file.path(datapath, "HCC1143_BL10.bam"))
 
-snp.plp::run_snp_pileup(vcffile = vcf, output = "test_test2.csv", bamfiles = bams, verbose = TRUE, debug_mode = TRUE, progress = TRUE)
+
+
+snp.plp::run_snp_pileup(vcffile = vcf, output = "test_test5", bamfiles = bams, verbose = TRUE, debug_mode = TRUE, progress = TRUE, gzipped = TRUE)
+ 
+
